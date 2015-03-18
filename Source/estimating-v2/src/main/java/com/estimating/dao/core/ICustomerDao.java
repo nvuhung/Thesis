79cd2f4548cfd1b8d90bda805eeeef588f7b2434
nvuhung <nvuhung@outlook.com>
2015-03-19 05:45:44 +0700

    Update ICustomerDao

--- Source/estimating-v2/src/main/java/com/estimating/dao/core/ICustomerDao.java
+++ Source/estimating-v2/src/main/java/com/estimating/dao/core/ICustomerDao.java
@@ -1,11 +1,12 @@
 package com.estimating.dao.core;
 
+import java.util.List;
+
 import com.estimating.entity.Customers;
 
 public interface ICustomerDao {
 	public void create(Customers customer);
-
 	public Customers findOneById(Long id);
-
 	public Customers findOneByName(String name);
+	public List<Customers> findAll();
 }
